ccd0c6227b9894f86cb71ce3f5f57459430650f3
Di M Dub <zensimilia@gmail.com>
2021-12-16 15:49:38 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let name = 'world';
+</script>
+
+<main class="w-[640px] mx-auto text-center">
+  <h1 class="font-bold text-2xl mt-4">Hello {name}!</h1>
+  <p class="mt-4">
+    Visit the <a
+      class="text-blue-600 underline-offset-1 underline"
+      href="https://svelte.dev/tutorial">Svelte tutorial</a
+    > to learn how to build Svelte apps.
+  </p>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
